fdf6adcb27f7be64f0b930e35c8fade8b47b3255
CarlosJimenez2003 <carlosjimenez@iesmontsia.org>
2023-12-04 09:58:16 +0100

    Inintial Commit

---
+++ provaM3.py
@@ -0,0 +1,7 @@
+a = int (input ('Ingresa el valor de a: '))
+b = int (input ('Ingresa el valor de b: '))
+c = int (input ('Ingresa el valor de c: '))
+suma=a+b+c
+print ('Valor de suma: ' + repr (suma))
+print ()
+    
